37f9ecd180b07a98691564e698e1bf8eb093e922
Tilen Majerle <tilen.majerle@gmail.com>
2023-12-17 12:21:48 +0100

    Fix GCC CMAKE compiler file

--- projects/usart_rx_polling_rtos_F4/cmake/gcc-arm-none-eabi.cmake
+++ projects/usart_rx_polling_rtos_F4/cmake/gcc-arm-none-eabi.cmake
@@ -4,12 +4,15 @@ set(CMAKE_SYSTEM_PROCESSOR          arm)
 # Some default GCC settings
 # arm-none-eabi- must be part of path environment
 set(TOOLCHAIN_PREFIX                arm-none-eabi-)
-set(FLAGS                           "-fdata-sections -ffunction-sections --specs=nano.specs -Wl,--gc-sections")
-set(CPP_FLAGS                       "-fno-rtti -fno-exceptions -fno-threadsafe-statics")
+set(FLAGS                           "-fdata-sections -ffunction-sections -Wl,--gc-sections")
+set(CPP_FLAGS                       "${FLAGS} -fno-rtti -fno-exceptions -fno-threadsafe-statics")
 
-set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc ${FLAGS})
+set(CMAKE_C_FLAGS                   ${FLAGS})
+set(CMAKE_CXX_FLAGS                 ${CPP_FLAGS})
+
+set(CMAKE_C_COMPILER                ${TOOLCHAIN_PREFIX}gcc)
 set(CMAKE_ASM_COMPILER              ${CMAKE_C_COMPILER})
-set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++ ${FLAGS} ${CPP_FLAGS})
+set(CMAKE_CXX_COMPILER              ${TOOLCHAIN_PREFIX}g++)
 set(CMAKE_OBJCOPY                   ${TOOLCHAIN_PREFIX}objcopy)
 set(CMAKE_SIZE                      ${TOOLCHAIN_PREFIX}size)
 
